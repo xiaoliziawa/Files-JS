087bc85ae230408f7458cd3a80c1ce179713301b
xiaoliziawa <3682822069@qq.com>
2024-12-07 23:46:52 +0800

    小修改

--- src/main/java/net/prizowo/filejs/kubejs/FileEventJS.java
+++ src/main/java/net/prizowo/filejs/kubejs/FileEventJS.java
@@ -1,14 +1,23 @@
 package net.prizowo.filejs.kubejs;
 
 import dev.latvian.mods.kubejs.event.EventJS;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 
 public class FileEventJS extends EventJS {
     private final String path;
     private final String content;
+    private final ServerPlayer player;
+    private final MinecraftServer server;
+    private final ServerLevel level;
 
-    public FileEventJS(String path, String content) {
+    public FileEventJS(String path, String content, ServerPlayer player, MinecraftServer server, ServerLevel level) {
         this.path = path;
         this.content = content;
+        this.player = player;
+        this.server = server;
+        this.level = level;
     }
 
     public String getPath() {
@@ -17,5 +26,17 @@ public class FileEventJS extends EventJS {
 
     public String getContent() {
         return content;
+    }
+
+    public ServerPlayer getPlayer() {
+        return player;
+    }
+
+    public MinecraftServer getServer() {
+        return server;
+    }
+
+    public ServerLevel getLevel() {
+        return level;
     }
 } 
